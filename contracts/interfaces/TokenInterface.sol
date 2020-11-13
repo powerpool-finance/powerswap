99f4358ce1de44bcef58209a10c788081fa4d775
Poli Paul <polipaul777@hotmail.com>
2020-11-13 16:09:30 +0200

    Reformat Solidity files with Prettier

--- contracts/interfaces/TokenInterface.sol
+++ contracts/interfaces/TokenInterface.sol
@@ -5,6 +5,7 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 abstract contract TokenInterface is IERC20 {
-    function deposit() public virtual payable;
-    function withdraw(uint) public virtual;
+  function deposit() public payable virtual;
+
+  function withdraw(uint256) public virtual;
 }
